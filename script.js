098c335c0942b332d742b8b20e2638af382743b0
Akshit Batra <akbatra567@gmail.com>
2020-08-17 21:19:50 +0530

    4x4 upgrade

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const WINNING_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
+  [0, 4, 8, 12],
+  [1, 5, 9, 13],
+  [2, 6, 10, 14],
+  [3, 7, 11, 15],
+  [0, 5, 10, 15],
+  [3, 6, 9, 12]
 ]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
